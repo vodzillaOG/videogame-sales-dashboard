f9e4b398f26a004ae74ba59cfd9d4d2b59d65543
Volodymyr Mykhayliv <volodymyr.m.95@gmail.com>
2023-05-22 13:15:03 +0100

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,8 @@ from dash import Dash
 
 
 # Data preprocessing
-df = pd.read_csv (data/vgsales.csv)
+url = "https://raw.githubusercontent.com/vodzillaOG/VG-sales/main/vgsales.csv"
+df = pd.read_csv(url).drop_duplicates()
 
 # Start the app
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
